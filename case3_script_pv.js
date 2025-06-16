bebf392c8a6acca5212a2c0b2bdc473badc39941
a-sazonov <92857091+a-sazonov@users.noreply.github.com>
2025-06-16 14:55:24 +0200

    Add files via upload

--- case3_script_pv.js
+++ case3_script_pv.js
@@ -1,25 +1,34 @@
 setTimeout(async () => {
   let fp;
-  const fpName = 'aidipfp';
+  const fpName = 'aidp_tt_fp';
   let ckPVCount;
   const ckPVCountName = 'aidp_tt_ckPVCount';
   let fpPVCount;
   const fpPVCountName = 'aidp_tt_fpPVCount';
+  let sCookieckPVCountVal;
+  let sCookiefpPVCountVal;
 
   try {
+    debugger
     fp = await window.adenty.scookie.get(fpName);
     ckPVCount = await window.adenty?.scookie.get(ckPVCountName);
+      sCookieckPVCountVal = Number(ckPVCount.value);
+
     fpPVCount = await window.adenty?.scookie.get(fpPVCountName);
+      sCookiefpPVCountVal = Number(fpPVCount.value);
+      debugger
   } catch (e) {
     fp = null;
     ckPVCount = null;
+    sCookieckPVCountVal = null;
     fpPVCount = null;
+    sCookiefpPVCountVal = null;
   }
 
   const date = new Date();
   date.setMonth(date.getMonth() + 1);
-
-  if (!ckPVCount || !fpPVCount || !fp) {
+debugger
+  if (!sCookieckPVCountVal || !sCookiefpPVCountVal || !fp) {
     document.cookie = `${ckPVCountName}=1; expires=${date.toUTCString()};`;
     window.adenty.scookie.set({
       name: ckPVCountName,
@@ -47,27 +56,51 @@ setTimeout(async () => {
   cookie.forEach(item => {
     let val;
     if (item.indexOf(ckPVCountCookiName) > -1) {
-      val = (item.trim().substring(ckPVCountCookiName.length) || '');
+      try {
+        val = (item.trim().substring(ckPVCountCookiName.length) || '');
+      } catch (e) {
+        val = null;
+      }
     }
+
     if (!val || val !== ckPVCount.value) {
       const expires = date.toUTCString();
+      window.adenty.event.fireServerEvent({name: 'VisitorCookieCountChanged', eventArguments: {[ckPVCountName]: ckPVCount}});
+
       window.adenty.scookie.set({
         name: ckPVCountName,
-        value: JSON.stringify(ckPVCount.value+1),
+        value: JSON.stringify(1),
         expires: date.toISOString(),
         purgeDate: date.toISOString()
       });
-      document.cookie = `${ckPVCountCookiName}=${ckPVCount.value+1}; expires=${expires};`;
+      document.cookie = `${ckPVCountCookiName}=${1}; expires=${expires};`;
+    } else {
+      window.adenty.scookie.set({
+        name: ckPVCountName,
+        value: JSON.stringify(sCookieckPVCountVal+1),
+        expires: date.toISOString(),
+        purgeDate: date.toISOString()
+      });
+      document.cookie = `${ckPVCountCookiName}=${sCookieckPVCountVal+1}; expires=${expires};`;
     }
   });
 
   if (fp?.value !== adenty.dl.adenty?.visit?.rid) {
+    window.adenty.event.fireServerEvent({name: 'VisitorFPCountChanged', eventArguments: {[fpPVCountName]: fpPVCount}});
+
     window.adenty.scookie.set({
       name: fpName,
       value: adenty.dl.adenty?.visit?.rid,
       expires: date.toISOString(),
       purgeDate: date.toISOString()
     });
+    window.adenty.scookie.set({
+      name: fpPVCountName,
+      value: JSON.stringify(1),
+      expires: date.toISOString(),
+      purgeDate: date.toISOString()
+    });
+  } else {
     window.adenty.scookie.set({
       name: fpPVCountName,
       value: JSON.stringify(ckPVCount+1),
